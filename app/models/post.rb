fc466f0bb5c843a33d02e57d3ac9326cd4d048d5
cato-hga <jchristopher.cato@gmail.com>
2016-06-03 23:59:27 -0400

    Implemented comments for Posts.

--- app/models/post.rb
+++ app/models/post.rb
@@ -10,6 +10,10 @@
 #
 
 class Post < ActiveRecord::Base
+  has_many :comments, dependent: :destroy
+
   validates :title, presence: true, length:{minimum: 3}
   validates :body, presence: true
+
+
 end
